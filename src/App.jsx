4c286ee30b4e99cfae978effb6423d79bbc8892c
Love Goyner <lovegoyner@gmail.com>
2025-06-15 19:25:46 +0530

    hero section completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+  
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+      <div className="h-dvh"></div>
+    </main>
+  );
+};
+
+export default App;
