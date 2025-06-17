41ca0d96be04a19a65c2f09c8c21ada9e46b7b95
Love Goyner <lovegoyner@gmail.com>
2025-06-17 17:12:10 +0530

    Contact and Footer section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Story from "./components/Story";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
       <About/>
       <Features/>
       <Story/>
+      <Contact/>
+      <Footer/>
     </main>
   );
 };
